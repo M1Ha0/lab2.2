1c6163b2713c1476304d58ef589ed35cf35222ce
MiHa0_o <MiHa0_o@209-5>
2024-10-14 11:51:58 +0200

    Добавьте файлы проекта.

---
+++ lab2.1/Program.cs
@@ -0,0 +1,16 @@
+﻿try
+{
+    Console.WriteLine("Введите трехзначное число");
+    int x = int.Parse(Console.ReadLine());
+    int c = x % 10;
+    int b = x % 100 / 10;
+    int a = x / 100;
+    if ((a==b)&&(b==c)) Console.WriteLine("Все цыфры одинаковы");
+    else Console.WriteLine("Цыфры не одинаковы");
+    if ((c == b) || (c == a) || (a == b)) Console.WriteLine("Есть одинаковые цыфры");
+    else Console.WriteLine("Нет одинаковых цыфры");
+}
+catch
+{
+    Console.WriteLine("Правильно введите данные");
+}
